efc22e38c820d30fbeea98bb134aae4bebfe676d
Adesoji Ajijolaoluwa <ajeeaidev@gmail.com>
2025-08-29 10:52:46 +0100

    First working version

---
+++ participant_pkg/main.py
@@ -0,0 +1,74 @@
+from pathlib import Path
+import file_ops
+workspace = Path("workspace")
+workspace.mkdir(exist_ok=True)
+path = workspace / "contacts.csv"
+participant_dict = {}
+
+while True:
+    while True:
+        try:
+            name = input("Enter your name: ")
+            if name == "":
+                continue
+            else:
+                print(f"Name is: {name}")
+                participant_dict["name"] = name
+                break
+                
+        except Exception as e:
+            print(f"The error was {e}")
+            
+    while True: 
+        try:
+            age = int(input("Enter your age: "))
+            if len(str(age)) == 0:
+                print("This field (age) cannot be left blank")
+                continue
+            else:
+                print(f"Age: {age}")
+                participant_dict["Age"] = age
+                break
+        except Exception as e:
+            print(f"The error was {e}")
+            
+    while True:
+        try:
+            phone_number = input("Enter your phone number: ")
+            if phone_number == "":
+                print("This field(phone_number) cannot be left empty")
+            elif len(phone_number) != 11:
+                print("Wrong length for phone number. The entry must be exactly 11 digits.")
+                continue
+                
+            else:
+                print(f"Phone number: {phone_number}")
+                participant_dict["Phone_number"] = phone_number
+                break
+        except Exception as e:
+            print("The error was {e}")
+    while True:            
+        try:
+            track = input("Enter your track: ")
+            if track == "":
+                print("This field (track) cannot be left blank")
+                continue
+            else:
+                print(f"Track: {track}")
+                participant_dict["Track"] = track
+                break
+            
+        except Exception as e:
+            print(f"The error was {e}")
+                
+    exit = input("Input \"done\" if you are done with your entries").title()
+                
+    file_ops.save_participant(path, participant_dict)
+    if exit == "Done":
+        break
+
+
+
+    # file_ops.save_participant(path, participant_dict)
+
+    # file_ops.load_participants(path)
